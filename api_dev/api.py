054a1fe950285fd156656438fe55641fe6d60365
Keniden <nik.glumov.94@mail.ru>
2024-04-10 12:44:43 +0300

    'update

--- api_dev/api.py
+++ api_dev/api.py
@@ -47,25 +47,30 @@ def vk_get_wall(id):
     token = os.getenv("TOKEN")
     version = 5.137
     user_id = id
-    offset = 0
     count_of_wall = 100
     post_type = "all"
     additional_fields = 1  # 1 = True, 0 = False
     #  1 — в ответе будут возвращены дополнительные поля profiles и groups,
     # содержащие информацию о пользователях и сообществах. По умолчанию: 0.
     additional_fields_params = "id"
-    response = requests.get(
-        "https://api.vk.com/method/wall.get",
-        params={
-            "access_token": token,
-            "v": version,
-            "owner_id": user_id,
-            "count": count_of_wall,
-            "filter": post_type,
-            "extended": additional_fields,
-            "fields": additional_fields_params,
-            "offset": dat,
-        },
-        timeout=100,
-    )
-    data = response.json()["response"]["items"]
+    all_posts = []
+    offset = 0
+    while offset < 1000:
+        response = requests.get(
+            "https://api.vk.com/method/wall.get",
+            params={
+                "access_token": token,
+                "v": version,
+                "owner_id": user_id,
+                "count": count_of_wall,
+                "filter": post_type,
+                "extended": additional_fields,
+                "fields": additional_fields_params,
+                "offset": offset,
+            },
+            timeout=100,
+        )
+        data = response.json()["response"]["items"]
+        offset += 100
+        all_posts.extend(data)
+    
