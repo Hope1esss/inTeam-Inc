b372b7bbb83db8ae5f12a7dd213c2157eeab824b
Keniden <nik.glumov.94@mail.ru>
2024-04-10 10:37:03 +0300

    update

--- api_dev/api.py
+++ api_dev/api.py
@@ -1,6 +1,6 @@
+import os
 import requests
 from dotenv import load_dotenv
-import os
 
 load_dotenv()
 
@@ -9,7 +9,14 @@ def vk_get_info(*ids):
     token = os.getenv("TOKEN")
     version = 5.89
     user_ids = " ,".join(ids)[:-1]
-    # fields = "activities,about,blacklisted,blacklisted_by_me,books,bdate,can_be_invited_group,can_post,can_see_all_posts,can_see_audio,can_send_friend_request,can_write_private_message,career,common_count,connections,contacts,city,country,crop_photo,domain,education,exports,followers_count,friend_status,has_photo,has_mobile,home_town,photo_100,photo_200,photo_200_orig,photo_400_orig,photo_50,sex,site,schools,screen_name,status,verified,games,interests,is_favorite,is_friend,is_hidden_from_feed,last_seen,maiden_name,military,movies,music,nickname,occupation,online,personal,photo_id,photo_max,photo_max_orig,quotes,relation,relatives,timezone,tv,universities"
+    # fields = "activities,about,blacklisted,blacklisted_by_me,books,bdate,can_be_invited_group,
+    # can_post,can_see_all_posts,can_see_audio,can_send_friend_request,can_write_private_message,
+    # career,common_count,connections,contacts,city,country,crop_photo,domain,education,exports,
+    # followers_count,friend_status,has_photo,has_mobile,home_town,photo_100,photo_200,
+    # photo_200_orig,photo_400_orig,photo_50,sex,site,schools,screen_name,status,verified,games,
+    # interests,is_favorite,is_friend,is_hidden_from_feed,last_seen,maiden_name,military,movies,
+    # music,nickname,occupation,online,personal,photo_id,photo_max,photo_max_orig,quotes,relation,
+    # relatives,timezone,tv,universities"
     fields = "activities,about"
     response = requests.get(
         "https://api.vk.com/method/users.get",
@@ -19,6 +26,7 @@ def vk_get_info(*ids):
             "user_ids": user_ids,
             "fields": fields,
         },
+        timeout=100,
     )
 
     data = response.json()
@@ -27,7 +35,7 @@ def vk_get_info(*ids):
         print(f"Имя: {data['response'][i]['first_name']}")
         print(f"Фамилия: {data['response'][i]['last_name']}")
         print(f"Id: {data['response'][i]['id']}")
-        print(f"******************************************")
+        print("******************************************")
     return data
 
 
