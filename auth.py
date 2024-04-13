eac25d54186738392728cdbac03d08caa92839fe
we1lman <andrey.velma2005@gmail.com>
2024-04-14 01:52:55 +0300

    added all docstings

--- auth.py
+++ auth.py
@@ -1,31 +1,150 @@
+"""
+Этот модуль предоставляет классы и методы для управления аутентификацией пользователей и
+шифрованием паролей.
+
+Классы
+-------
+Encryption
+    Обрабатывает шифрование и проверку паролей с использованием bcrypt.
+AuthenticationSystem
+    Управляет аутентификацией пользователей, включая добавление новых пользователей с
+    зашифрованными паролями и проверку входов в систему.
+
+Класс `Encryption` использует bcrypt для безопасного хеширования и проверки паролей,
+в то время как класс `AuthenticationSystem` взаимодействует с базой данных для
+хранения и проверки учетных данных пользователей.
+
+Примеры
+--------
+Чтобы создать новый экземпляр AuthenticationSystem и использовать его для добавления нового
+пользователя и проверки входа:
+
+>>> auth_system = AuthenticationSystem()
+>>> auth_system.adding_user('ilya.goat@yandex.ru', 'iaml00s3r')
+>>> auth_system.login('ilya.goat@yandex.ru', 'iaml00s3r')
+True
+
+Примечания
+-----
+Убедитесь, что модуль `database_script` доступен в той же среде, что и этот модуль,
+поскольку `AuthenticationSystem` зависит от него для управления данными пользователя.
+"""
+
 import bcrypt
 import database_script
 
 
 class Encryption:
+    """
+    Класс для работы с шифрованием паролей с использованием bcrypt.
+
+    Параметры
+    ----------
+    password : str
+        Пароль в открытом виде, который будет зашифрован.
+
+    Атрибуты
+    ----------
+    salt : bytes
+        Криптографическая "соль", сгенерированная для хеширования.
+    hashed_password : bytes
+        Хешированный пароль, сохраненный в виде байтов.
+
+    Методы
+    -------
+    check_password(password)
+        Проверяет пароль в открытом виде против зашифрованного хеша.
+    get_hashed_password()
+        Возвращает зашифрованный пароль.
+    """
+
     def __init__(self, password):
         self.salt = bcrypt.gensalt()
         self.hashed_password = bcrypt.hashpw(password.encode('utf-8'), self.salt)
 
     def check_password(self, password):
-        if bcrypt.checkpw(password.encode('utf-8'), self.hashed_password):
-            return True
-        return False
+        """
+        Проверяет пароль в открытом виде против зашифрованного хеша.
+
+        Параметры
+        ----------
+        password : str
+            Пароль в открытом виде для проверки.
+
+        Возвращает
+        -------
+        bool
+            True, если пароль совпадает с хешем, иначе False.
+        """
+        return bcrypt.checkpw(password.encode('utf-8'), self.hashed_password)
 
     def get_hashed_password(self):
+        """
+        Возвращает зашифрованный пароль.
+
+        Возвращает
+        -------
+        bytes
+            Хешированный пароль, сохраненный в виде байтов.
+        """
         return self.hashed_password
 
 
 class AuthenticationSystem:
+    """
+    Класс, отвечающий за управление аутентификацией пользователей.
+
+    Атрибуты
+    ----------
+    database : Database
+        Экземпляр Database из модуля database_script,
+        используемый для управления данными пользователя.
+
+    Методы
+    -------
+    adding_user(login, password)
+        Добавляет нового пользователя с зашифрованным паролем в базу данных.
+    login(login, password)
+        Проверяет учетные данные пользователя и возвращает True, если вход успешен.
+    """
+
     def __init__(self):
         self.database = database_script.Database()
 
     def adding_user(self, login, password):
+        """
+        Добавляет нового пользователя с зашифрованным паролем в базу данных.
+
+        Параметры
+        ----------
+        login : str
+            Логин пользователя.
+        password : str
+            Пароль пользователя в открытом виде, который будет зашифрован перед сохранением.
+
+        Возвращает
+        -------
+        None
+        """
         hashed_password = Encryption(password).get_hashed_password()
         self.database.add_user(login, hashed_password)
 
     def login(self, login, password):
-        if self.database.get_login(login) == login and \
-                bcrypt.checkpw(password.encode('utf-8'), self.database.get_password(login)):
-            return True
-        return False
+        """
+        Проверяет учетные данные пользователя и возвращает True, если вход успешен.
+
+        Параметры
+        ----------
+        login : str
+            Логин пользователя.
+        password : str
+            Пароль пользователя в открытом виде для проверки.
+
+        Возвращает
+        -------
+        bool
+            True, если учетные данные пользователя верны и вход выполнен успешно, иначе False.
+        """
+        user_login = self.database.get_login(login)
+        user_password = self.database.get_password(login)
+        return user_login == login and bcrypt.checkpw(password.encode('utf-8'), user_password)
--- database_script.py
+++ database_script.py
@@ -1,8 +1,50 @@
+"""
+Этот модуль предоставляет класс Database для управления базой данных пользователей.
+Класс позволяет добавлять новых пользователей, получать пароли и логины, а также
+закрывать соединение с базой данных.
+
+Классы
+-------
+Database
+    Управляет соединением с базой данных SQLite и предоставляет методы для работы с
+    таблицей пользователей.
+
+Исключения
+----------
+UserAlreadyExistsError
+    Вызывается, когда попытка добавить пользователя с существующим логином.
+"""
+
 import sqlite3
 import errors
 
 
 class Database:
+    """
+    Класс для управления базой данных пользователей через SQLite.
+
+    При инициализации создает подключение к базе данных и таблицу пользователей,
+    если она не существует.
+
+    Атрибуты
+    ----------
+    connection : Connection
+        Объект подключения к базе данных SQLite.
+    cursor : Cursor
+        Объект курсора для выполнения SQL команд.
+
+    Методы
+    -------
+    add_user(login, password)
+        Добавляет нового пользователя в базу данных.
+    get_password(login)
+        Возвращает пароль пользователя по логину.
+    get_login(login)
+        Проверяет наличие логина в базе данных.
+    finish_connection()
+        Закрывает подключение к базе данных.
+    """
+
     def __init__(self):
         self.connection = sqlite3.connect("database.db")
         self.cursor = self.connection.cursor()
@@ -12,25 +54,75 @@ class Database:
         """)
 
     def add_user(self, login, password):
+        """
+        Добавляет нового пользователя в базу данных.
+
+        Параметры
+        ----------
+        login : str
+            Логин нового пользователя.
+        password : str
+            Пароль нового пользователя.
+
+        Исключения
+        ----------
+        UserAlreadyExistsError
+            Вызывается, если пользователь с таким логином уже существует.
+        """
+
         try:
-            self.cursor.execute("INSERT INTO users(login, password) VALUES(?, ?);", (login, password))
+            self.cursor.execute("INSERT INTO users(login, password) VALUES(?, ?);",
+                                (login, password))
         except sqlite3.IntegrityError as exc:
-            raise errors.UserAlreadyExistsError("User already exists") from exc
+            raise errors.UserAlreadyExistsError(
+                f"Имя пользователя {login} уже используется."
+            ) from exc
         self.connection.commit()
 
     def get_password(self, login):
+        """
+        Возвращает пароль пользователя по его логину.
+
+        Параметры
+        ----------
+        login : str
+            Логин пользователя.
+
+        Возвращает
+        -------
+        str или bool
+            Пароль пользователя или False, если логин не найден.
+        """
         try:
-            password = self.cursor.execute("SELECT password FROM users WHERE login =?;", (login,)).fetchone()[0]
+            password = self.cursor.execute("SELECT password FROM users WHERE login =?;", \
+                                           (login,)).fetchone()[0]
         except TypeError:
             return False
         return password
 
     def get_login(self, login):
+        """
+        Проверяет наличие логина в базе данных и возвращает его.
+
+        Параметры
+        ----------
+        login : str
+            Логин для проверки.
+
+        Возвращает
+        -------
+        str или bool
+            Логин, если найден, иначе False.
+        """
         try:
-            login = self.cursor.execute("SELECT login FROM users WHERE login =?;", (login,)).fetchone()[0]
+            login = self.cursor.execute("SELECT login FROM users WHERE login =?;", \
+                                        (login,)).fetchone()[0]
         except TypeError:
             return False
         return login
 
     def finish_connection(self):
+        """
+        Закрывает подключение к базе данных.
+        """
         self.connection.close()
