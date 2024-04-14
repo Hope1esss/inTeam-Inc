7486c2ea570fee812d4d9e70a3ced8acd36bf1f1
Keniden <nik.glumov.94@mail.ru>
2024-04-14 19:47:51 +0300

    fix posts parser

--- api.py
+++ api.py
@@ -12,7 +12,7 @@ class Api:
         self.id = id
         self.token = os.getenv("TOKEN")
 
-    def VKUserInfo(self):
+    def vk_user_info(self):
         version = 5.89
         user_ids = self.id
         ru = 0
@@ -38,19 +38,20 @@ class Api:
         )
 
         data = response.json()
-        id = data["response"][0]["id"]
-        bdate = data["response"][0].get("bdate", None)
-        city = data["response"][0].get("city", {}).get("title", None)
-        university_name = data["response"][0]["university_name"]
-        ret = self.DatabaseWriter(
-            id,
-            bdate,
-            None if city == "" else city,
-            None if university_name == "" else university_name,
+        id_data = data["response"][0]["id"]
+        sex_data = data["response"][0]["sex"]
+        bdate_data = data["response"][0].get("bdate", None)
+        city_data = data["response"][0].get("city", {}).get("title", None)
+        university_name_data = data["response"][0]["university_name"]
+        self.data_base_writer(
+            id_data,
+            sex_data,
+            bdate_data,
+            None if city_data == "" else city_data,
+            None if university_name_data == "" else university_name_data,
         )
-        return ret
 
-    def VKWallPosts(self):
+    def vk_wall_posts(self):
         version = 5.137
         user_id = self.id
         count_of_wall = 100
@@ -61,44 +62,54 @@ class Api:
         additional_fields_params = "id"
         all_posts = []
         offset = 0
-        while offset < 100:
-            response = requests.get(
-                "https://api.vk.com/method/wall.get",
-                params={
-                    "access_token": self.token,
-                    "v": version,
-                    "owner_id": user_id,
-                    "count": count_of_wall,
-                    "filter": post_type,
-                    "extended": additional_fields,
-                    "fields": additional_fields_params,
-                    "offset": offset,
-                },
-                timeout=100,
-            )
-            data = response.json()["response"]["items"]
-            offset += 100
-            all_posts.extend(data)
+        response = requests.get(
+            "https://api.vk.com/method/wall.get",
+            params={
+                "access_token": self.token,
+                "v": version,
+                "owner_id": user_id,
+                "count": count_of_wall,
+                "filter": post_type,
+                "extended": additional_fields,
+                "fields": additional_fields_params,
+                "offset": offset,
+            },
+            timeout=100,
+        )
+        data = response.json()["response"]["items"]
+        all_posts.extend(data)
         return all_posts
 
-    def VKPostsExporter(self):
-        with open("file1.cvs", "w", encoding="utf-8") as file:
+    def vk_posts_exporter(self):
+        os.remove("file1.csv")
+        with open("file1.csv", "w", encoding="utf-8") as file:
             pen = csv.writer(file)
-            pen.writerow(["likes", "body", "url"])
-            for post in self.vk_get_posts():
-                try:
-                    if post["attachments"][0]:
-                        img_url = post["attachments"][0]["photo"]["sizes"][-1]["url"]
+            pen.writerow(["likes", "views", "text", "img-url", "audio-url"])
+            for post in self.vk_wall_posts():
+                img_url = []
+                audio_url = []
+                for i in range(len(post["attachments"])):
+                    if post["attachments"][i]["type"] == "photo":
+                        img_url.append(
+                            post["attachments"][i]["photo"]["sizes"][-1]["url"]
+                        )
+                    if post["attachments"][i]["type"] == "audio":
+                        audio_url.append(post["attachments"][i]["audio"]["url"])
+                    if post["text"] == "":
+                        text = "None"
                     else:
-                        img_url = "pass"
-                except:
-                    pass
-                try:
-                    pen.writerow([post["likes"]["count"], post["text"], img_url])
-                except:
-                    pass
+                        text = post["text"]
+                pen.writerow(
+                    [
+                        post["likes"]["count"],
+                        post["views"]["count"] if "views" in post.keys() else "0",
+                        text,
+                        img_url,
+                        audio_url,
+                    ]
+                )
 
-    def DatabaseWriter(self, id, date_of_birth, city, education):
+    def data_base_writer(self, id, sex, date_of_birth, city, education):
         try:
             database = sqlite3.connect("user.db")
             cursor = database.cursor()
@@ -106,15 +117,16 @@ class Api:
             cursor.execute(
                 """CREATE TABLE IF NOT EXISTS users (
             id integer PRIMARY KEY,
+            sex integer,
             bdate text,
             city text,
             education text
         )"""
             )
             sqlite_insert_with_param = """INSERT INTO users
-                                (id, bdate, city, education)
-                                VALUES (?, ?, ?, ?)"""
-            data_tuple = (id, date_of_birth, city, education)
+                                (id, sex, bdate, city, education)
+                                VALUES (?, ?, ?, ?, ?)"""
+            data_tuple = (id, sex, date_of_birth, city, education)
             cursor.execute(sqlite_insert_with_param, data_tuple)
             database.commit()
             print("Переменные Python успешно вставлены в таблицу user.db")
@@ -125,8 +137,7 @@ class Api:
             if database:
                 database.close()
                 print("Соединение с SQLite закрыто")
-        return 0
 
 
 user1 = Api(os.getenv("ID"))
-user1.VKUserInfo()
+user1.vk_posts_exporter()
