9b2afd90f7e3d1886a0599d17c0b3141872343eb
finlleyl <ilya_chibugaev@mail.ru>
2024-04-19 19:28:16 +0300

    Improved /me response

--- app/api/v1/endpoints/data.py
+++ app/api/v1/endpoints/data.py
@@ -1,10 +1,11 @@
 from fastapi import APIRouter, Depends
 from app.api.db.init_db import User
+from app.api.schemas.user import User as UserSchema
 from app.api.services.user_service import get_current_user
 
 router = APIRouter()
 
 
-@router.get("/me")
+@router.get("/me", response_model=UserSchema)
 async def read_users_me(current_user: User = Depends(get_current_user)):
     return current_user
