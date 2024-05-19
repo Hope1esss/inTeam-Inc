43a330a788f5f88ecbecf3ae3a57e930f2c2d44c
we1lman <andrey.velma2005@gmail.com>
2024-05-20 02:17:59 +0300

    fixed api.py GLOBAL UPDATE

--- app/api/db/session.py
+++ app/api/db/session.py
@@ -2,9 +2,9 @@ from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession
 
 from app.api.db.init_db import engine
 
-async_sesion = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+async_session = async_sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
 
 
 async def get_session():
-    async with async_sesion() as session:
+    async with async_session() as session:
         yield session
