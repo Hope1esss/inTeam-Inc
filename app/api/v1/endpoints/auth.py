946856d5ee2f844c8385878e03d922bb9598e3da
finlleyl <ilya_chibugaev@mail.ru>
2024-05-20 01:43:41 +0300

    added router

--- app/api/v1/endpoints/auth.py
+++ app/api/v1/endpoints/auth.py
@@ -18,11 +18,11 @@ async def register(user: UserCreate, session: AsyncSession = Depends(get_session
     return {"message": "User created successfully", "user_id": new_user.id}
 
 
-@router.post("/login")
+@router.post("/login", response_model=dict)
 async def login(
     response: Response, user: UserLogin, session: AsyncSession = Depends(get_session)
 ):
     user = await login_user(user_data=user, session=session)
     access_token = create_access_token(data={"sub": str(user.id)})
-    response.set_cookie(key="access_token", value=access_token)
-    return {"message": "Login successful"}
+    response.set_cookie(key="access_token", value=access_token, path="/")
+    return {'message': 'Login successful'}
