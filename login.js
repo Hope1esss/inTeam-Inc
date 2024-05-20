444a379f182d6466dacc28f013044a8bab966459
finlleyl <ilya_chibugaev@mail.ru>
2024-05-20 22:31:12 +0300

    edited style.css

---
+++ login.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    const vkLoginButton = document.getElementById('vk-login-btn');
+    vkLoginButton.addEventListener('click', redirectToVKAuth);
+});
+
+function redirectToVKAuth() {
+    const client_id = '51899044'; // Замените на ваш client_id
+    const redirect_uri = encodeURIComponent('https://4894-195-123-219-158.ngrok-free.app/callback.html'); // URL, на который будет передан code
+    const display = 'page'; // Отображение в отдельном окне
+    const scope = 'friends'; // Права доступа, которые вы хотите запросить
+    const response_type = 'code'; // Тип ответа - code
+    const v = '5.131'; // Версия API
+
+    const authUrl = `https://oauth.vk.com/authorize?client_id=${client_id}&display=${display}&redirect_uri=${redirect_uri}&scope=${scope}&response_type=${response_type}&v=${v}`;
+    window.location.href = authUrl;
+}
--- main.js
+++ main.js
@@ -1,19 +1,9 @@
-document.addEventListener('DOMContentLoaded', async () => {
-    const registerForm = document.getElementById('registerForm');
-    const loginForm = document.getElementById('loginForm');
-
-    const userInfo = document.getElementById('user-info');
-    const userAvatar = document.getElementById('user-avatar');
-    const userName = document.getElementById('user-name');
-    const authButtons = document.getElementById('auth-buttons');
-
-
+async function checkToken() {
     const cookies = document.cookie.split('; ').reduce((prev, current) => {
         const [name, ...value] = current.split('=');
         prev[name] = value.join('=');
         return prev;
     }, {});
-
 
     if (cookies.access_token) {
         try {
@@ -27,91 +17,34 @@ document.addEventListener('DOMContentLoaded', async () => {
             }
 
             const data = await response.json();
-            userName.textContent = data.username;
-            userAvatar.textContent = data.username.slice(0, 2).toUpperCase();
-
-            userInfo.style.display = 'flex';
-            authButtons.style.display = 'none';
+            return data;
         } catch (error) {
             console.error('Error during token check:', error);
-            document.getElementById('message').textContent = error.message;
+            return null;
         }
+    } else {
+        return null;
+    }
+}
+
+
+document.addEventListener('DOMContentLoaded', async () => {
+    const userInfo = document.getElementById('user-info');
+    const userAvatar = document.getElementById('user-avatar');
+    const userName = document.getElementById('user-name');
+    const authButtons = document.getElementById('auth-buttons');
+
+    const userData = await checkToken();
+
+    if (userData) {
+        userName.textContent = userData.username;
+        userAvatar.textContent = userData.username[0].toUpperCase();
+
+        userInfo.style.display = 'flex';
+        authButtons.style.display = 'none';
     }
 
-
-    if (registerForm) {
-        registerForm.addEventListener('submit', async (event) => {
-            event.preventDefault();
-
-            const username = document.getElementById('username').value;
-            const password = document.getElementById('password').value;
-
-            // Проверка наличия токена при загрузке стран
-
-            try {
-                const response = await fetch('http://localhost:8000/api/v1/auth/register', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({username, password}),
-                    credentials: 'include'
-                });
-
-                const data = await response.json();
-                if (!response.ok) {
-                    if (response.status === 422) {
-                        const errors = data.detail.map(e => `${e.loc[1]}: ${e.msg}`).join('\n');
-                        throw new Error(errors);
-                    } else if (response.status === 400 && data.detail === "Username already exists") {
-                        throw new Error("Username already exists. Please choose another one.");
-                    } else {
-                        throw new Error(data.detail || 'Registration failed');
-                    }
-                }
-                document.getElementById('message').textContent = data.message;
-
-                window.location.href = 'index.html';
-            } catch (error) {
-                document.getElementById('message').textContent = error.message;
-            }
-        });
-    }
-
-
-    if (loginForm) {
-        loginForm.addEventListener('submit', async (event) => {
-            event.preventDefault();
-            const username = document.getElementById('username').value;
-            const password = document.getElementById('password').value;
-
-            try {
-                const response = await fetch('http://localhost:8000/api/v1/auth/login', {
-                    method: 'POST',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({username, password}),
-                    credentials: 'include'
-                });
-
-                const data = await response.json();
-                if (!response.ok) {
-                    if (response.status === 422) {
-                        const errors = data.detail.map(e => `${e.loc[1]}: ${e.msg}`).join('\n');
-                        throw new Error(errors);
-                    } else if (response.status === 400 && data.detail === "Invalid username or password") {
-                        throw new Error("Invalid username or password. Please try again.");
-                    } else {
-                        throw new Error(data.detail || 'Login failed');
-                    }
-                }
-                document.getElementById('message').textContent = data.message;
-
-                window.location.href = 'index.html';
-            } catch (error) {
-                document.getElementById('message').textContent = error.message;
-            }
-        });
+    if (userData && (window.location.pathname === '/sign.html' || window.location.pathname === '/login.html')) {
+        window.location.href = 'index.html';
     }
 });
