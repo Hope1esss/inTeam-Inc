1fdf32c82be30994ccb283721b867525c84eafff
finlleyl <ilya_chibugaev@mail.ru>
2024-06-01 01:26:28 +0300

    fixed login

--- sign.js
+++ sign.js
@@ -1,21 +1,18 @@
 import CONFIG from "./config.js";
 
-
 document.addEventListener('DOMContentLoaded', async () => {
-    const vkLoginButton = document.getElementById('vk-login-btn');
-    vkLoginButton.addEventListener('click', redirectToVKAuth);
-    console.log('VK Login button event listener added');
+    const vkRegisterButton = document.getElementById('vk-login-btn');
+    vkRegisterButton.addEventListener('click', redirectToVKAuth);
 });
 
 function redirectToVKAuth() {
     const client_id = CONFIG.CLIENT_ID; // Замените на ваш client_id
-    const redirect_uri = CONFIG.REDIRECT_URI; // URL, на который будет передан code
+    const redirect_uri = `${CONFIG.REDIRECT_URI}`; // URL, на который будет передан code и режим
     const display = 'page'; // Отображение в отдельном окне
     const scope = 'friends'; // Права доступа, которые вы хотите запросить
     const response_type = 'code'; // Тип ответа - code
     const v = '5.131'; // Версия API
 
     const authUrl = `https://oauth.vk.com/authorize?client_id=${client_id}&display=${display}&redirect_uri=${encodeURIComponent(redirect_uri)}&scope=${scope}&response_type=${response_type}&v=${v}`;
-    console.log('Redirecting to VK Auth URL:', authUrl);
     window.location.href = authUrl;
 }
