76d1abb6ec02ce2e06ce43353523f09d8ca6b5fa
finlleyl <ilya_chibugaev@mail.ru>
2024-05-31 21:56:57 +0300

    reworked vk auth

--- app/api/v1/endpoints/__init__.py
+++ app/api/v1/endpoints/__init__.py
@@ -3,9 +3,11 @@ from fastapi import APIRouter
 from .auth import router as auth_router
 from .dashboard import router as dashboard_router
 from .data import router as data_router
+from .token import router as token_router
 
 
 router = APIRouter()
 router.include_router(auth_router, prefix="/auth", tags=["auth"])
 router.include_router(dashboard_router, prefix="/dashboard", tags=["dashboard"])
 router.include_router(data_router, prefix="/data", tags=["data"])
+router.include_router(token_router, prefix='/token', tags=['vk_token'])
