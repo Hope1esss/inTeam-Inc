e784ff87e1d2a87788b5f11b9e9234c9d990fa46
Keniden <nik.glumov.94@mail.ru>
2024-06-01 16:21:53 +0300

    new import router

--- app/api/v1/endpoints/user.py
+++ app/api/v1/endpoints/user.py
@@ -1,3 +1,4 @@
+from app.api.services.api import Api
 from fastapi import APIRouter, HTTPException, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 from app.api.db.session import get_session
@@ -9,18 +10,16 @@ from app.api.services.user_service import (
     get_current_user,
 )
 import asyncio
-from app.api.api import Api  # Импортируем ваш парсер
 
-router = APIRouter()
+user_router = APIRouter()
 
 
-@router.post("/get_id", response_model=dict)
+@user_router.post("/get_id", response_model=dict)
 async def get_user_id(user: UserCreate, db: AsyncSession = Depends(get_session)):
-    # Ваш код здесь
     pass
 
 
-@router.post("/user_info/{user_id}", response_model=dict)
+@user_router.post("/user_info/{user_id}", response_model=dict)
 async def get_user_info(user_id: str):
     api = Api(user_id)
     await Api.async_db_setup()
@@ -31,7 +30,7 @@ async def get_user_info(user_id: str):
         raise HTTPException(status_code=500, detail=str(e))
 
 
-@router.post("/wall_posts/{user_id}", response_model=dict)
+@user_router.post("/wall_posts/{user_id}", response_model=dict)
 async def get_wall_posts(user_id: str):
     api = Api(user_id)
     await Api.async_db_setup()
