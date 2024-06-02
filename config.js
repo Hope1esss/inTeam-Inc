cf06707e4862a8f44491ac44d7ebb7711fe1d636
finlleyl <ilya_chibugaev@mail.ru>
2024-06-03 00:12:48 +0300

    updated responses

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-    REDIRECT_URI: 'https://24ee-194-54-176-84.ngrok-free.app/callback.html',
+    REDIRECT_URI: 'https://opazjuhet5.ru/callback.html',
     CLIENT_ID: '51937099',
     RESPONSE: 'localhost'
 };
